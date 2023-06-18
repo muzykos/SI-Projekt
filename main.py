71ea19f20af3bfa7a75d259c08ccb1f2d5fd4d72
papryku <supteros@gmail.com>
2023-06-18 19:51:50 +0200

    progress %

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 import pandas as pd
-import csv 
+import csv
+import nltk
+nltk.download('wordnet') 
 from nltk.stem import WordNetLemmatizer
 import re
-from wordProcesser import emojis, stopwordlist, preprocess
+from wordProcesser import preprocess
 
 def parsePolarity(polarity):
     if not polarity:
@@ -28,7 +30,7 @@ test_labels = []
 
 p = int(sample_count / test_count)
 
-
+print("Reading twitter_emotions.csv...")
 with open('twitter_emotions.csv', newline='', encoding="ISO-8859-1") as f:
     reader = csv.reader(f)
     for ir, row in enumerate(reader):
@@ -51,13 +53,16 @@ with open('twitter_emotions.csv', newline='', encoding="ISO-8859-1") as f:
                         train_data.append(data[0])
                         train_labels.append(data[1])
                     continue
- 
-print("Total number of samples: ", sample_count, "\nWanted number of tests: ", test_count, 
-      "\nActual number of tests: ", len(test_data), "\nNumber of train samples: ", len(train_data))
+        if ir % 10000 == 0:
+            print((ir*100)/1600000,"% complete...")
+print("Reading complete.") 
+
+print("Total number of samples: ", sample_count, ".\nWanted number of tests: ", test_count, 
+      ".\nActual number of tests: ", len(test_data), ".\nNumber of train samples: ", len(train_data),".")
 
 # Vectorizing text data
 vectorizer = TfidfVectorizer()
-train_features = vectorizer.fit_transform(train_text)
+train_features = vectorizer.fit_transform(train_data)
 test_features = vectorizer.transform(test_data)
 
 # Training the classifier
@@ -67,9 +72,9 @@ classifier.fit(train_features, train_labels)
 # Predicting test samples
 predictions = classifier.predict(test_features)
 
-# Print the predictions
-# for text, prediction in zip(test_data, predictions):
-#     print(f"Text: {text} | Sentiment: {prediction}")
+# Print the predictions (uncommenting would make it take moore time)
+for text, prediction in zip(test_data, predictions):
+    print(f"Text: {text} | Sentiment: {prediction}")
 
 # Evaluating the accuracy of the classifier
 accuracy = accuracy_score(test_labels, predictions)
