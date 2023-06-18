99e821b12f46055a4f8aacb394862b41db40889f
papryku <105929571+papryku@users.noreply.github.com>
2023-06-18 19:32:06 +0200

    here we go, basic implementation
    download twitter_emotions.csv

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
-from samples import samples
-import csv
+import pandas as pd
+import csv 
+from nltk.stem import WordNetLemmatizer
+import re
+from wordProcesser import emojis, stopwordlist, preprocess
 
 def parsePolarity(polarity):
     if not polarity:
@@ -12,26 +15,26 @@ def parsePolarity(polarity):
     return -1
 
 # Write here what % of samples should be in test_data   
-test_part = 0.1
+test_part = 0.2
 # example_limit = 1200
 
 sample_count = 1600000
 test_count = int(sample_count * test_part)
 
 train_data = []
+train_labels = []
 test_data = []
 test_labels = []
 
 p = int(sample_count / test_count)
 
-with open('twitter_emotions.csv', newline='') as f:
+
+with open('twitter_emotions.csv', newline='', encoding="ISO-8859-1") as f:
     reader = csv.reader(f)
     for ir, row in enumerate(reader):
         data = []
-        print(row)
         for ic, column in enumerate(row):
             if ic in [0, 5]:
-                # print(column, ic, end=',')
                 if not parsePolarity:
                     continue
                 if not ic:
@@ -39,52 +42,35 @@ with open('twitter_emotions.csv', newline='') as f:
                     if(parsed):
                         data.append(parsed)
                 else:
-                    data.append(column)
+                    data.append(preprocess(column))
                     data = data[::-1]
-            if len(data) == 2:
-                if(ir % p == 0):
-                    test_data.append(data[0])
-                    test_labels.append(data[1])
-                else:
-                    train_data.append(data)
+                    if(ir % p == 0):
+                        test_data.append(data[0])
+                        test_labels.append(data[1])
+                    else:
+                        train_data.append(data[0])
+                        train_labels.append(data[1])
+                    continue
+ 
+print("Total number of samples: ", sample_count, "\nWanted number of tests: ", test_count, 
+      "\nActual number of tests: ", len(test_data), "\nNumber of train samples: ", len(train_data))
 
-# sample_count = len(samples)
-# test_count = int(sample_count * test_part)
-
-# p = int(sample_count / test_count)
-
-# for id, sample in enumerate(samples):
-#     if id % p == 0:
-#         test_data.append(sample[0])
-#         test_labels.append(sample[1])
-#     else:
-#         train_data.append(sample)
-
-print(sample_count, test_count, len(test_data), len(train_data))
-
-# Preprocess the training data
-train_text = [data[0] for data in train_data]
-train_labels = [data[1] for data in train_data]
-
-# Preprocess the test data
-test_text = test_data
-
-# Vectorize the text data
+# Vectorizing text data
 vectorizer = TfidfVectorizer()
 train_features = vectorizer.fit_transform(train_text)
-test_features = vectorizer.transform(test_text)
+test_features = vectorizer.transform(test_data)
 
-# Train the classifier
+# Training the classifier
 classifier = MultinomialNB()
 classifier.fit(train_features, train_labels)
 
-# Make predictions on the test data
+# Predicting test samples
 predictions = classifier.predict(test_features)
 
 # Print the predictions
-# for text, prediction in zip(test_text, predictions):
+# for text, prediction in zip(test_data, predictions):
 #     print(f"Text: {text} | Sentiment: {prediction}")
 
-# Evaluate the accuracy of the classifier
+# Evaluating the accuracy of the classifier
 accuracy = accuracy_score(test_labels, predictions)
 print(f"Accuracy: {accuracy}")
---
+++ wordProcesser.py
@@ -0,0 +1,63 @@
+from nltk.stem import WordNetLemmatizer
+import re
+
+emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink', ':-E': 'vampire', ':(': 'sad', 
+          ':-(': 'sad', ':-<': 'sad', ':P': 'raspberry', ':O': 'surprised',
+          ':-@': 'shocked', ':@': 'shocked',':-$': 'confused', ':\\': 'annoyed', 
+          ':#': 'mute', ':X': 'mute', ':^)': 'smile', ':-&': 'confused', '$_$': 'greedy',
+          '@@': 'eyeroll', ':-!': 'confused', ':-D': 'smile', ':-0': 'yell', 'O.o': 'confused',
+          '<(-_-)>': 'robot', 'd[-_-]b': 'dj', ":'-)": 'sadsmile', ';)': 'wink', 
+          ';-)': 'wink', 'O:-)': 'angel','O*-)': 'angel','(:-D': 'gossip', '=^.^=': 'cat'}
+
+stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
+             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
+             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
+             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from', 
+             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
+             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
+             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're',
+             's', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
+             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+             'themselves', 'then', 'there', 'these', 'they', 'this', 'those', 
+             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
+             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
+             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
+             "youve", 'your', 'yours', 'yourself', 'yourselves']
+
+def preprocess(tweet):
+    # Create Lemmatizer and Stemmer.
+    wordLemm = WordNetLemmatizer()
+    
+    # Defining regex patterns.
+    urlPattern        = r"((http://)[^ ]*|(https://)[^ ]*|( www\.)[^ ]*)"
+    userPattern       = '@[^\s]+'
+    alphaPattern      = "[^a-zA-Z0-9]"
+    sequencePattern   = r"(.)\1\1+"
+    seqReplacePattern = r"\1\1"
+    
+    tweet = tweet.lower()
+        
+    # Replace links with URL
+    tweet = re.sub(urlPattern,' URL',tweet)
+    # Replace listed basic emojis
+    for emoji in emojis.keys():
+        tweet = tweet.replace(emoji, "EMOJI" + emojis[emoji])        
+    # Replace @USERNAME with USER.
+    tweet = re.sub(userPattern,' USER', tweet)        
+    # Replace ching chang chong
+    tweet = re.sub(alphaPattern, " ", tweet)
+    # Shorten 3 or more letters to just 2
+    tweet = re.sub(sequencePattern, seqReplacePattern, tweet)
+
+    tweetwords = ''
+    for word in tweet.split():
+        # Checking if the word is a stopword.
+        #if word not in stopwordlist:
+        if len(word)>1:
+            # Lemmatizing the word.
+            word = wordLemm.lemmatize(word)
+            tweetwords += (word+' ')
+            
+    return tweetwords
