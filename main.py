a53eb170b834f82f6bbb40a5a11c33222b296c30
papryku <105929571+papryku@users.noreply.github.com>
2023-06-18 18:33:43 +0200

    yes
    can't push twitter_emotions.csv

--- main.py
+++ main.py
@@ -1,65 +1,90 @@
-import nltk
-import numpy as np
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
-# from frlearn import FuzzyRoughClassifier
-# from frlearn.reducts import get_reducts
+from samples import samples
+import csv
 
-# Step 1: Data Preprocessing
-nltk.download("stopwords")
-nltk.download("punkt")
-nltk.download("wordnet")
+def parsePolarity(polarity):
+    if not polarity:
+        return 'negative'
+    elif polarity == 2:
+        return 'positive'
+    return -1
 
-stop_words = set(stopwords.words("english"))
-lemmatizer = WordNetLemmatizer()
+# Write here what % of samples should be in test_data   
+test_part = 0.1
+# example_limit = 1200
 
-# Example text data
-text_data = [
-    ("I love this movie", "positive"),
-    ("This book is boring", "negative"),
-    ("Great experience at the restaurant", "positive"),
-    ("The service was terrible", "negative")
-]
+sample_count = 1600000
+test_count = int(sample_count * test_part)
 
-preprocessed_data = []
-for text, label in text_data:
-    text = text.lower()
-    tokens = word_tokenize(text)
-    preprocessed_text = [
-        lemmatizer.lemmatize(word) for word in tokens if word not in stop_words
-    ]
-    preprocessed_data.append((" ".join(preprocessed_text), label))
+train_data = []
+test_data = []
+test_labels = []
 
-# Step 2: Feature Extraction
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform([data[0] for data in preprocessed_data])
-y = np.array([data[1] for data in preprocessed_data])
+p = int(sample_count / test_count)
 
-# Step 3: Rough Set Theory with fuzzy-rough-learn
-# X = X.toarray()  # Convert the sparse matrix X to a dense array
+with open('twitter_emotions.csv', newline='') as f:
+    reader = csv.reader(f)
+    for ir, row in enumerate(reader):
+        data = []
+        print(row)
+        for ic, column in enumerate(row):
+            if ic in [0, 5]:
+                # print(column, ic, end=',')
+                if not parsePolarity:
+                    continue
+                if not ic:
+                    parsed = parsePolarity(int(column))
+                    if(parsed):
+                        data.append(parsed)
+                else:
+                    data.append(column)
+                    data = data[::-1]
+            if len(data) == 2:
+                if(ir % p == 0):
+                    test_data.append(data[0])
+                    test_labels.append(data[1])
+                else:
+                    train_data.append(data)
 
-# reducts = get_reducts(X, y)
+# sample_count = len(samples)
+# test_count = int(sample_count * test_part)
 
-# classifier = FuzzyRoughClassifier()
-# classifier.set_reducts(reducts)
-# classifier.fit(X, y)
+# p = int(sample_count / test_count)
 
-# Step 4: Classification
-# Make predictions and evaluate the performance
-preprocessed_test_data = [
-    ("I really enjoyed the movie", "positive"),
-    ("The book was not interesting", "negative")
-]
+# for id, sample in enumerate(samples):
+#     if id % p == 0:
+#         test_data.append(sample[0])
+#         test_labels.append(sample[1])
+#     else:
+#         train_data.append(sample)
 
-X_test = vectorizer.transform([data[0] for data in preprocessed_test_data]).toarray()
-y_test = np.array([data[1] for data in preprocessed_test_data])
+print(sample_count, test_count, len(test_data), len(train_data))
 
-# predictions = classifier.predict(X_test)
-# accuracy = accuracy_score(y_test, predictions)
-# print("Accuracy:", accuracy)
-print(X_test, y_test)
+# Preprocess the training data
+train_text = [data[0] for data in train_data]
+train_labels = [data[1] for data in train_data]
+
+# Preprocess the test data
+test_text = test_data
+
+# Vectorize the text data
+vectorizer = TfidfVectorizer()
+train_features = vectorizer.fit_transform(train_text)
+test_features = vectorizer.transform(test_text)
+
+# Train the classifier
+classifier = MultinomialNB()
+classifier.fit(train_features, train_labels)
+
+# Make predictions on the test data
+predictions = classifier.predict(test_features)
+
+# Print the predictions
+# for text, prediction in zip(test_text, predictions):
+#     print(f"Text: {text} | Sentiment: {prediction}")
+
+# Evaluate the accuracy of the classifier
+accuracy = accuracy_score(test_labels, predictions)
+print(f"Accuracy: {accuracy}")
